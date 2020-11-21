ac48be335da8f41c567bb38177fa4fc0ae787763
wiktorkaminski <wiktor.kaminski@mixbox.pl>
2020-11-21 15:21:52 +0100

    initial commit

---
+++ src/main/java/pl/wiktorkaminski/basicinvoiceapp/BasicInvoiceApp.java
@@ -0,0 +1,13 @@
+package pl.wiktorkaminski.basicinvoiceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicInvoiceApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicInvoiceApp.class, args);
+    }
+
+}
